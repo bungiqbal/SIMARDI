f3d42b876114cf514605078ace7289c511b19ec8
bung iqbal <iqbalpasker71@gmail.com>
2024-05-06 22:00:55 +0700

    get

--- app/Views/index.php
+++ app/Views/index.php
@@ -83,12 +83,6 @@
   <!-- ?PROD Only: Google Tag Manager (noscript) (Default ThemeSelection: GTM-5DDHKGP, PixInvent: GTM-5J3LMKC) -->
   <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5DDHKGP" height="0" width="0" style="display: none; visibility: hidden"></iframe></noscript>
   <!-- End Google Tag Manager (noscript) -->
-  
-  
-
-
-
-
 
 <script src="<?php echo base_url('') ?>assets/vendor/js/dropdown-hover.js"></script>
 <script src="<?php echo base_url('') ?>assets/vendor/js/mega-dropdown.js"></script>
