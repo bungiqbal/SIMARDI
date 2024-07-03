7088f5489d43db66c69f9c37dbefbd7330f21f79
bung iqbal <iqbalpasker71@gmail.com>
2024-07-03 09:17:28 +0700

    09.17

--- app/Views/admin/layout/header.php
+++ app/Views/admin/layout/header.php
@@ -88,9 +88,10 @@
 
     <!-- Jquery -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="<?php echo base_url('') ?>vendor/libs/jquery/jquery.js"></script>
-    <script src="<?php echo base_url('') ?>vendor/phpunit/php-code-coverage/src/Report/Html/Renderer/Template/js/jquery.min.js"></script>
-    
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="<?php echo base_url('') ?>assets/vendor/libs/jquery/jquery.js"></script>
+    <script src="<?php echo base_url('') ?>assets/vendor/libs/jquery/jquery-3.7.1.min.js"></script>
+    <script src="<?php echo base_url('') ?>assets/vendor/phpunit/php-code-coverage/src/Report/Html/Renderer/Template/js/jquery.min.js"></script>
 
 </head>
 
